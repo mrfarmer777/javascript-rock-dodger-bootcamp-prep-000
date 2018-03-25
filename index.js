7381016327dc5b839f4adbb387b370a0100ff408
Matt Farmer <mrfarmer777@gmail.com>
2018-03-25 14:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -150,10 +150,10 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if(e.which===LEFT_ARROW){
-     console.log(e.which);
+     console.log("moving left: "+e.which);
      moveDodgerLeft();
    } else  if(e.which===RIGHT_ARROW){
-     console.log(e.which);
+     console.log("moving right: "+e.which);
      moveDodgerRight();
    }
 
