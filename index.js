fa3daeb177d180f1b9e0b119824b3cb0fa078216
Matt Farmer <mrfarmer777@gmail.com>
2018-03-25 13:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  clearInterval(gameInterval);
+  window.clearInterval(gameInterval);
   for(var r=0; r<ROCKS.length; r++){
     ROCKS[r].remove;
   };
